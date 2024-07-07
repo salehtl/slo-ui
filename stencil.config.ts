1375e933a19e6bb57794c5eef59789bbf9294af2
Saleh <salehtl@icloud.com>
2024-07-07 12:23:22 +0400

    Stencil Initial Setup

---
+++ stencil.config.ts
@@ -0,0 +1,27 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'slo-ui',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+  globalStyle: 'src/global/global.css',
+};
