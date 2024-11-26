134d5285706969b84022aa0e9a8b522b6db68970
saleh <salehtl@icloud.com>
2024-11-26 19:38:01 +0400

    Ugly button commit

---
+++ stencil.config.ts
@@ -0,0 +1,23 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'slo-ui',
+  globalStyle: 'src/shared/styles.css',
+  outputTargets: [
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
